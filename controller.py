fdc63d58db9e4b760d04b11ef6ff514772388a0d
henningolweus <holweus@gmail.com>
2024-02-06 16:14:50 +0100

    Last commit

--- controller.py
+++ controller.py
@@ -1,37 +1,28 @@
 import json
 import jax.numpy as jnp
-from jax import vmap
-from collections import deque
-import numpy as np
 
-# Load the configuration file
-with open('config.json', 'r') as file:
-    config = json.load(file)
-
-# Accessing a specific configuration setting
 
 class ClassicController:
     def __init__(self, learning_rate, params):
+        # Initialize controller with learning rate and parameters
         self.learning_rate = learning_rate
         self.error_history = []
         self.params = jnp.array(params)
-    
-    def update_params(self, grads):
-        params -= self.learning_rate * grads
-        self.error_history = []
-        return params
 
     def compute_control_signal(self, current_error):
+        # Compute control signal based on current error
         self.error_history.append(current_error)
-        if len(self.error_history) >= 2:
+        if len(self.error_history) > 1:
+            # Compute derivative of error if history is long enough
             error_de = current_error - self.error_history[-2]
         else:
             error_de = 0  # Set error_de to 0 if there are fewer than 2 elements in error_history
-        error_sum = jnp.sum(jnp.array(self.error_history))
-        error_array = jnp.array([current_error, error_sum, error_de])
-        return jnp.dot(self.params, error_array)
+        error_sum = jnp.sum(jnp.array(self.error_history))  # Sum of errors for integral component
+        error_array = jnp.array([current_error, error_sum, error_de])  # Error components array
+        return jnp.dot(self.params, error_array)  # Calculate control signal
     
     def loss_function(self):
+        # Calculate mean squared error (MSE) of error history
         return jnp.mean(jnp.array(self.error_history)**2)
     
 
@@ -41,30 +32,34 @@ class ClassicController:
 
 class NeuralNetworkController():
     def __init__(self, learning_rate, activation_functions, params):
+        # Initialize NN controller with learning rate, activation functions, and parameters
         self.activation_functions = activation_functions
         self.learning_rate = learning_rate
         self.error_history = []
-        self.params = [(jnp.array(w), jnp.array(b)) for w, b in params]
+        self.params = [(jnp.array(w), jnp.array(b)) for w, b in params] # Convert params to JAX numpy array for tracing
 
     def compute_control_signal(self, current_error):
+        # Compute control signal based on current error
         self.error_history.append(current_error)
         error_sum = jnp.sum(jnp.array(self.error_history))
         if len(self.error_history)>1:
             error_de = current_error - self.error_history[-2]
         else:
             error_de = 0
+        # Array of current error, sum of errors, and error difference
         error_array = jnp.array([current_error, error_sum, error_de])
         
         return self.predict(error_array)
 
     def predict(self, features):
+        # Predict output based on input features using the neural network
         activations = jnp.array(features)
         for i, (weights, biases) in enumerate(self.params):
             # Apply the specified activation function for the current layer
             activation_func = self.get_activation_function(i)
             activations = jnp.dot(activations, jnp.array(weights)) + jnp.array(biases)
             activations = activation_func(activations)
-        return jnp.squeeze(activations)
+        return jnp.squeeze(activations) # Remove extra dimensions
     
     def get_activation_function(self, layer_index):
         # Get the activation function based on the layer index
@@ -82,9 +77,11 @@ class NeuralNetworkController():
             raise ValueError(f"No activation function specified for layer {layer_index}")
     
     def loss_function(self):
+        # Compute mean squared error of error history as loss
         return jnp.mean(jnp.array(self.error_history)**2)
     
     def relu(x):
+        # Rectified Linear Unit activation function
         return jnp.maximum(0, x)
 
     # Sigmoid activation function
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import random
 from plant import Bathtub, CournotCompetition, LogisticGrowthPlant
 from controller import ClassicController, NeuralNetworkController
 import jax
@@ -6,60 +5,69 @@ import jax.numpy as jnp
 import numpy as np
 import json
 import matplotlib.pyplot as plt
-from jax import jit
 
 
 class ConSys:
     def __init__(self, config):
+        # Initialize control system with configuration settings
         self.config = config
 
     def run_one_epoch(self, params, disturbance_array):
+        # Simulate one epoch (cycle) of the control system
         disturbance_array = jnp.array(disturbance_array)
         control_signal = 0
-        controller = self.init_controller(params)
+        controller = self.init_controller(params) # Initialize controller with parameters
         plant = self.init_plant() 
-        output_history = []
 
+        # Loop through each timestep in the epoch
         for i in range(self.config["simulation"]["timesteps_per_epoch"]):
+            # Update plant state and compute error
             output = plant.update_state(control_signal,disturbance_array[i])
             error = self.config["simulation"]["target_value"] - output
+            # Compute new control signal based on error
             control_signal = controller.compute_control_signal(error)
-            output_history.append(output)
-
-        return controller.loss_function(), output_history
+        
+        # Return MSE for the epoch
+        return controller.loss_function()
         
     def run_system(self):
-        gradfunc = jax.value_and_grad(self.run_one_epoch, argnums=0,has_aux=True) #computes both the value and gradient of self.run_one_epoch (which returns MSE from loss function) with respect to its first argument
-        params = self.init_params() #initializes the parameters of the controller from the config file
+        # Main loop to run the control system simulation
+        gradfunc = jax.value_and_grad(self.run_one_epoch, argnums=0) # Setup for automatic differentiation
+        params = self.init_params() # Initialize parameters based on config
         kp_history, ki_history, kd_history, mse_history = [], [], [], [], 
+        learning_rate = self.config["simulation"]["learning_rate"]
 
+        # Run simulation for configured number of epochs
         for i in range(self.config["simulation"]["num_epochs"]):
+            # Generate disturbance array for the epoch
             disturbance_array = np.random.uniform(-0.01, 0.01, self.config["simulation"]["timesteps_per_epoch"])
-            (mse, output_history), grads = gradfunc(params, disturbance_array) #Calculates the MSE for the current epoch, and the gradients of the MSE with respect to the parameters
+            # Compute MSE and gradients for current epoch
+            mse, grads = gradfunc(params, disturbance_array) 
             mse_history.append(mse)
             # Update parameters
-            learning_rate = self.config["simulation"]["learning_rate"]
             params = self.update_params(params, grads, learning_rate)
+            # Track PID parameters if using classic controller
             if self.config["controller"]["type"] == "classic":
                 kp_history.append(params[0])
                 ki_history.append(params[1])
                 kd_history.append(params[2])
+            # Print progress
             print("MSE")
             print(mse)
             print("epoch: " + str(i+1) + "/" + str(self.config["simulation"]["num_epochs"]))
         
+        # Plotting histories for analysis
         self.plot_history([kp_history, ki_history, kd_history],'PID Parameters over Epochs','Epoch','Parameter Value',['kp', 'ki', 'kd'])
         self.plot_history(mse_history, 'Mean Squared Error over Epochs', 'Epoch', 'MSE Value', ['MSE'])
-        self.plot_history(output_history, 'Output History over Timesteps', 'Timestep', 'Output', ['Output per timestep'])
 
     def update_params(self, params, grads, learning_rate, clip_value=1.0):
-        # Gradient clipping function
-        #calculate the Euclidean norm of the gradient vector
+        # Update parameters with gradient descent and gradient clipping
+        # Gradient clipping wqs introduced to deal with reocurring NaN-outputs from gradfunc
         def clip_gradient(gradient):
-
             norm = jnp.linalg.norm(gradient)
             return jnp.where(norm > clip_value, gradient * clip_value / norm, gradient)
-
+        
+        # Update for neural network or classical controller parameters
         if isinstance(params, list) and isinstance(params[0], list):  # Check if params is a list of lists (neural network)
             #Check if params is a list of lists (neural network)
             updated_params = []
@@ -79,19 +87,27 @@ class ConSys:
             return params - learning_rate * clipped_grads
 
     def gen_neural_net_params(self):
+        # Generate initial parameters for neural network controller
+
         layers = self.config["controller"]["neural_net"]["layers"]
         weight_range = self.config["controller"]["neural_net"]["weight_initial_range"]
         bias_range = self.config["controller"]["neural_net"]["bias_initial_range"]
         layers = self.config["controller"]["neural_net"]["layers"]
         sender = layers[0]; params = []
+        # Initialize weights and biases for each layer  
         for receiver in layers[1:]:
+            # Initialize weights and biases for the current layer with the shape specified in the config file
             weights = np.random.uniform(weight_range[0],weight_range[1], (sender, receiver))
             biases = np.random.uniform(bias_range[0], bias_range[1],(1,receiver))
+            # Append the weights and biases for the current layer to the parameters list
             params.append([weights, biases])
+            # Update the sender size for the next layer's connections
             sender = receiver
+        # Return the complete list of parameters (weights and biases) for all layers
         return params
     
     def init_params(self):
+        # Initialize parameters, check if the controller is "classic" or "neural_net"
         if self.config["controller"]["type"] == "classic":
             params = jnp.array([config["controller"]["classic"]["kp"],config["controller"]["classic"]["ki"], config["controller"]["classic"]["kd"] ])
         elif self.config["controller"]["type"] == "neural_net":
@@ -101,6 +117,7 @@ class ConSys:
         return params
     
     def init_controller(self, params):
+        # Initialize controller with the given params, check if the controller is classic or neural_net
         if self.config["controller"]["type"] == "classic":
             controller = ClassicController( config['simulation']['learning_rate'], params)
         elif self.config["controller"]["type"] == "neural_net":
@@ -111,6 +128,7 @@ class ConSys:
         return controller
     
     def init_plant(self):
+        # Initialize the plant based on the specified plant in the config file
         if self.config["plant"]["type"] == "bathtub":
             plant = Bathtub(self.config)
         elif self.config["plant"]["type"] == "cournot_competition":
@@ -123,7 +141,6 @@ class ConSys:
 
     def plot_history(self, series_list, title, x_label, y_label, legend_labels):
         plt.figure(figsize=(12, 5))
-
         # Check if it's a single series or multiple series
         if isinstance(series_list[0], list):
             # Plotting each series in the list
